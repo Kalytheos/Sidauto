591810e2f5fc24a2e6d7bd670f138ea5ee1789a4
Dorian Esguerra <dorianjosephes@gmail.com>
2026-02-17 21:34:50 -0500

    Delete env.js

--- env.js
+++
@@ -1,9 +0,0 @@
-// Variables de entorno para la aplicación
-window.ENV = {
-  SUPABASE_URL: 'https://rxlfqzxcdloabakrqoed.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4bGZxenhjZGxvYWJha3Jxb2VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4NzY4NTksImV4cCI6MjA3ODQ1Mjg1OX0.DLZc7rCSZCyx6UKfpOg3Kwm7BPsQ2ZJ2ZeWvi1eTRso'
-};
-
-console.log('Variables de Supabase configuradas correctamente');
-
-// I know .env shouldn´t be here, but because this is not the real webpage which is allocated in a private server I don´t know if it matters
