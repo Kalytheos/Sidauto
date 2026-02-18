344e0d8d8bc4ced8fc21081f8b6d522c2ffbfb8d
Dorian Esguerra <dorianjosephes@gmail.com>
2026-02-17 21:35:37 -0500

    Remove Supabase configuration comments
    Removed configuration comments for Supabase.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Configuración de Supabase
 const SUPABASE_URL = 'https://rxlfqzxcdloabakrqoed.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4bGZxenhjZGxvYWJha3Jxb2VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4NzY4NTksImV4cCI6MjA3ODQ1Mjg1OX0.DLZc7rCSZCyx6UKfpOg3Kwm7BPsQ2ZJ2ZeWvi1eTRso';
 
